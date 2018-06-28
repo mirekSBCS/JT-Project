868cf10fdb5fd6d5700ba6676c7a86f032b2df56
mirekSBCS <33669390+mirekSBCS@users.noreply.github.com>
2018-06-28 12:18:49 -0400

    6/28

---
+++ Project 3D #1/Assets/CollectableScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectableScript : MonoBehaviour {
+
+	HUD_Manager hudmanager;
+	public int points;
+	
+	// Use this for initialization
+	void Start () {
+		hudmanager = GameObject.FindGameObjectWithTag("Player").GetComponent<HUD_Manager>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+void OnTriggerEnter(Collider other)
+{
+	if (other.gameObject.CompareTag("Player")) {
+		gameObject.SetActive(false);
+		hudmanager.UpdateScore(points);
+		}
+
+	}
+
+}
---
+++ Project 3D #1/Assets/HitPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlayer : MonoBehaviour {
+
+	public int AttackDamage;
+
+	void OnTriggerEnter(Collider other){
+		HUD_Manager hudmanager = other.GetComponent<HUD_Manager>();
+
+		hudmanager.UpdateHealth(AttackDamage);
+	}
+}
